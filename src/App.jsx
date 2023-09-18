ad0057f549b2578d38f66ec741b39ceb3a430015
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-18 11:02:27 +0800

    basic styles, font family added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
 function App() {
-  return <div>a</div>;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          padding: "20px",
+          borderRadius: "30px",
+        }}
+      >
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <h1>Stay updated!</h1>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+
+          <div>
+            <p>Product discovery and building what matters</p>
+            <p>Measuring to ensure updates are a success</p>
+            <p>And much more!</p>
+          </div>
+
+          <input type="email" style={{ width: "100%" }} />
+          <button style={{ width: "100%" }}>
+            Subscribe to monthly newsletter
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
